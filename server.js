c6e662e21516b7631370934edd54058d6fdeb8a8
Mateo Molina <molinamateo447@gmail.com>
2017-08-26 15:08:24 -0500

    creo el metodo restar, sumar,multiplicar,dividir

--- server.js
+++ server.js
@@ -5,21 +5,71 @@ var server =express();
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
 
-server.get("/login", function(){
+server.get("/suma", function(req,res){
+	var parametro2=parseInt(req.query.param2);
+	var parametro=parseInt(req.query.param);
+	var hola=suma(parametro,parametro2);
+	res.json(hola);
 
 })
+server.get("/resta", function(req,res){
+	var parametro2=parseInt(req.query.param2);
+	var parametro=parseInt(req.query.param);
+	var hola=resta(parametro,parametro2);
+	res.json(hola);
+
+})
+server.get("/multiplicacion", function(req,res){
+	var parametro2=parseInt(req.query.param2);
+	var parametro=parseInt(req.query.param);
+	var hola=multiplicacion(parametro,parametro2);
+	res.json(hola);
+
+})
+server.get("/division", function(req,res){
+	var parametro2=parseInt(req.query.param2);
+	var parametro=parseInt(req.query.param);
+	var hola=division(parametro,parametro2);
+	res.json(hola);
+
+})
+
 
 server.listen(3000, function(){
 	console.log("servidor corriendo")
 
 });
 
-function suma(parametro,Numero){
+//var _node_name  = document.getElementById("nodename").value;
+
+function suma(Numero1,Numero2){
 	var total =0;
-	var suma = parametro + Numero;
-	for (var i = 5; i >= 0; i--) {
-		total = total + suma;
+	
+		total = Numero1+Numero2;
+			return total;
 	}	
+
+
+
+function resta(Numero1,Numero2){
+	var total=0;
+	total=Numero1-Numero2;
+	return total;
+
+}
+
+function multiplicacion(Numero1,Numero2){
+	var total=0;
+	total=Numero1*Numero2;
 	return total;
 }
+
+function division(Numero1,Numero2){
+	var total=0;
+	total=Numero1/Numero2;
+	return total;
+
+}
+
+
 
